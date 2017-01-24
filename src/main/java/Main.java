8f3c6b507d7c582215e266f9138948fc80773db2
Joe Kutner <jpkutner@gmail.com>
2017-01-24 13:00:05 -0600

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) throws Exception {
+
+    // Hit Heroku API to download a slug file
+
+    // TODO unpack the tar ball
+
+    // Launch a Java process
+  }
+}
